eb7a345adc4ce9d13b78191e7f87a8d94f7cee01
kevinf96 <kevin.fink@hotmail.de>
2019-01-29 20:34:25 +0100

    search optimized and bootstrap

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -5,15 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(items: ShopItem[], searchText: string, leagueSelect: number) {
+  transform(items: any[], searchText: string, leagueSelect: number) {
     if(!items) return [];
-    if(!searchText) return items;
     console.log(leagueSelect);
     searchText = searchText.toString().toLowerCase();
-    items =  items.filter(v => v.name.toLowerCase().indexOf(searchText) >= 0);
-    if(leagueSelect > 0)
-      items =  items.filter(v => v.category == leagueSelect);
-    return items;
+    items = (!searchText) ? items : items.filter(v => v.name.toLowerCase().indexOf(searchText) >= 0);
+    items =  (leagueSelect > 0) ? items.filter(v => v.category == leagueSelect) : items;
+    return items; 
   }
 
 }
